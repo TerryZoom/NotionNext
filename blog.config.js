ac74532234df4595b741feaa080d6dd26d809045
SorryImCute <trzm63@gmail.com>
2026-05-10 03:23:05 +0800

    Update author bio in blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '伊茗雪', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '深度学习、AIGC、游戏、艺术的四灵根散修，不专业的人工智能本科生，分享自己的学习、创作与生活', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '深度学习、AIGC、游戏、艺术的四灵根散修', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.86601125.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '伊茗雪, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/chole.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
